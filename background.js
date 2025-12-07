8a9a2dddaa1db5b9f34b847d666b1696cb9b25d0
llong2195 <nduylong9501@gmail.com>
2025-12-08 01:09:16 +0700

    refactor: Remove unused background and content scripts, and clean up CSS styles

--- background.js
+++
@@ -1,6 +0,0 @@
-// Background Service Worker
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('YouTube Link Extractor installed.');
-});
-
-// We can add keyboard shortcuts or context menu items here in the future
